c9c412b3693e9c580978f81a677b4412d6e8b6f0
mabaqs <mabaqs@gmail.com>
2025-04-09 11:25:24 -0300

    Create questions.ts

---
+++ lib/questions.ts
@@ -0,0 +1,7 @@
+export const questions = [
+  { question: "¿Con qué frecuencia te has sentido triste en los últimos 7 días?" },
+  { question: "¿Has tenido problemas para dormir o permanecer dormido/a?" },
+  { question: "¿Has sentido que las cosas que solías disfrutar ya no te interesan?" },
+  { question: "¿Con qué frecuencia has tenido pensamientos negativos sobre ti mismo/a?" },
+  { question: "¿Has tenido dificultad para concentrarte en tus tareas diarias?" }
+];
