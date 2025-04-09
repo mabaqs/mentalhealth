9024f447c77454e8efa71d43560627b509b74cc5
mabaqs <mabaqs@gmail.com>
2025-04-09 11:31:24 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const isGithubPages = process.env.GITHUB_PAGES === 'true';
 
 const nextConfig = {
   output: 'export',
-  basePath: isGithubPages ? '/nombre-del-repo' : '',
-  assetPrefix: isGithubPages ? '/nombre-del-repo/' : '',
+  basePath: isGithubPages ? '/mentalhealth' : '',
+  assetPrefix: isGithubPages ? '/mentalhealth/' : '',
 };
 
 module.exports = nextConfig;
